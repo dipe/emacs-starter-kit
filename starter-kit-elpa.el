aab4704bb3b743a886a4c5ce8a0b485e970393bd
Peter Ehrenberg <pe@flat.adh29>
2010-02-06 15:11:50 +0100

    update

--- starter-kit-elpa.el
+++ starter-kit-elpa.el
@@ -7,6 +7,7 @@
                                    'inf-ruby
                                    'css-mode
                                    'gist)
+
   "Libraries that should be installed by default.")
 
 (defun starter-kit-elpa-install ()
@@ -39,6 +40,6 @@ just have to assume it's online."
 
 ;; Workaround for an ELPA bug that people are reporting but I've been
 ;; unable to reproduce:
-(autoload 'paredit-mode "paredit")
+;;(autoload 'paredit-mode "paredit")
 
 (provide 'starter-kit-elpa)
